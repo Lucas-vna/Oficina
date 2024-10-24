b312d308788e8d57a29e78e7fae62d907112434a
Lucas dos Reis Viana <55816219+Lucas-vna@users.noreply.github.com>
2024-10-24 12:25:17 -0300

    Add files via upload

---
+++ decorações.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="decorações" tilewidth="32" tileheight="32" tilecount="42" columns="7">
+ <image source="Decorations (32x32).png" width="224" height="192"/>
+</tileset>
